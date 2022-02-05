1d2a1ea9fc283e043f754bcbdcb9e43266b88b54
clurkwel <artemiykovinko@gmail.com>
2022-02-05 17:40:03 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const URL = "https://web-app-papatomatoe.herokuapp.com";
+
+const postList = document.querySelector(".posts__list");
+const postTitleValue = document.querySelector(".value");
+const btn = document.querySelector(".posts__button");
+const btn2 = document.querySelector("add-post__button")
+const addPostTitle
+const p = document.querySelector(".posts__loader")
+const inputTitle = document.querySelector(".add-post__input");
+const addPostContent = document.querySelector(".add-post__content");
+const addPostButton = document.querySelector(".add-post__button");
+
+const getPostMarkup = (post) => {
+    const postItem = document.createElement("li");
+    const postTitle = document.createElement("h3");
+    const postContent = document.createElement("p");
+    const postDate = document.createElement("time");
+
+    postTitle.innerText = post.title;
+    postContent.innerText = post.content;
+    postDate.innerText = post.createDate;
+    postDate.setAttribute("datetime", post.createDate);
+    
+    postItem.classList.add("posts__item");
+    postTitle.classList.add("posts__title");
+    postContent.classList.add("posts__content");
+    postDate.classList.add("posts__date");
+
+    postItem.append(postTitle);
+    postItem.append(postContent);
+    postItem.append(postDate);
+
+    return postItem;
+};
+
+const checkForm = () => {
+    if (inputTitle.value == "" || addPostContent.value == "") {
+      if (addPostButton.hasAttribute("disabled")) {
+      } else {
+        addPostButton.setAttribute("disabled", "disabled");
+      }
+    } else {
+      addPostButton.removeAttribute("disabled");
+    }
+  };
+
+const fragment = new DocumentFragment();
+
+const getPosts = () => {
+    p.classList.add("visually-hidden")
+    fetch(`${URL}/posts`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((posts) => {
+            posts.forEach((post) => {
+                const postElement = getPostMarkup(post);
+                fragment.append(postElement);
+            });
+
+            postTitleValue.innerText = posts.length;
+            postList.append(fragment);
+        })
+        .catch((error) => console.error(error));
+};
+
+
+
+btn.addEventListener("click", getPosts);
